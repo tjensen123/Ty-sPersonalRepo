18dca1290fbd6b0771c7fe25c68c4e05d79c3602
Ty Jesen <ty.jensen.123@gmail.com>
2017-10-07 03:00:58 -0600

    Add project files.

---
+++ ClassLibrary1/Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        private Stack<char> Operators;
+        private Stack<double> Operands;
+        private int level;
+
+        public Calculator()
+        {
+            Operators = new Stack<char>();
+            Operands = new Stack<double>();
+            level = 0;
+        }
+
+        public double Evaluate()
+        {
+            switch (level)
+            {
+                case 1:
+
+            }
+        }
+
+        public double Operate()
+        {
+            double  a = Operands.Pop();
+            double b = Operands.Pop();
+
+
+            switch (Operators.Pop())
+            {
+                case '+':
+                    
+            }
+            
+        }
+
+        public void level1()
+        {
+
+        }
+
+    }
+}
---
+++ ClassLibrary1/Levels.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class Levels
+    {
+        private Stack<string> Operators;
+        private Stack<double> Operands;
+        string[] op = new string[9] { "+", "-", "*", "/", "<=", ">=", "%","(",")" };
+        private int level;
+
+        public Levels(int _level)
+        {
+            Operators = new Stack<string>();
+            Operands = new Stack<double>();
+
+            level = _level;
+
+        }   
+
+        public Boolean CreateValues()
+        {
+            Random rnd = new Random();
+            switch (level)
+            {
+                case 1:
+                    Operands.Push(rnd.Next(1, 9));
+                    Operands.Push(rnd.Next(1, 9));
+                    Operators.Push(op[rnd.Next(0, 4)]);
+                    break;
+
+                    
+            }
+            return true;
+        }
+
+        public ISet<double> GetOperandStack()
+        {
+            HashSet<double> result = new HashSet<double>(Operands);
+            return result;
+        }
+
+        public ISet<string> GetOperatorStack()
+        {
+            HashSet<string> result = new HashSet<string>(Operators);
+            return result;
+        }
+    }
+}
